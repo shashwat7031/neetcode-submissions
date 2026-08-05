dbc032102f865917d1c6c1040ca8763925f388a2
shashwat7031 <shashwatjha384@gmail.com>
2026-08-05 22:40:09 +0530

    Add: sql-create-table - submission-2

---
+++ SQL/sql-create-table/submission-2.sql
@@ -0,0 +1,8 @@
+create table videos(id int,name text,created_at date,published Boolean);
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
